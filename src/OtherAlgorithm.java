14a90d9d1da14fc97cd6f92063ca8a92d7e8f4ca
AmirHossein-nr <arazlighi@gmail.com>
2024-12-07 18:16:29 +0330

    improved version of the matrix multiplication class

--- src/OtherAlgorithm.java
+++ src/OtherAlgorithm.java
@@ -21,10 +21,18 @@ public class OtherAlgorithm {
 
     private static double[][] multiplyMatrices(double[][] matrixA, double[][] matrixB, int size) {
         double[][] result = new double[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                for (int k = 0; k < size; k++) {
-                    result[i][j] += matrixA[i][k] * matrixB[k][j];
+        int blockSize = 64;
+
+        for (int i = 0; i < size; i += blockSize) {
+            for (int j = 0; j < size; j += blockSize) {
+                for (int k = 0; k < size; k += blockSize) {
+                    for (int ii = i; ii < Math.min(i + blockSize, size); ii++) {
+                        for (int jj = j; jj < Math.min(j + blockSize, size); jj++) {
+                            for (int kk = k; kk < Math.min(k + blockSize, size); kk++) {
+                                result[ii][jj] += matrixA[ii][kk] * matrixB[kk][jj];
+                            }
+                        }
+                    }
                 }
             }
         }
